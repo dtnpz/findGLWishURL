da1ea02cb5862de4bc0970c8506a47a2f54793e1
hayato <nattapang.p@gmail.com>
2025-03-25 08:19:53 +0700

    edit variable

--- genshinWishURL.py
+++ genshinWishURL.py
@@ -1,22 +1,22 @@
 import re
 import subprocess
 
-file_path = "Genshin Impact game/GenshinImpact_Data/webCaches/2.34.0.0/Cache/Cache_Data/data_2"
+filePath = "Genshin Impact game/GenshinImpact_Data/webCaches/2.34.0.0/Cache/Cache_Data/data_2"
 pattern = r"https://gs\.hoyoverse\.com/genshin/event/e20190909gacha-v3/.*?&game_biz=hk4e_global"
 
-def getLastMatchURL(file_path, pattern):
+def getLastMatchURL(filePath, pattern):
     lastMatch = None
-    with open(file_path, "r", encoding="utf-8", errors="ignore") as file:
+    with open(filePath, "r", encoding="utf-8", errors="ignore") as file:
         for line in file:
             matches = re.findall(pattern, line)
             if matches:
                 lastMatch = matches[-1]
     return lastMatch
 
-last_url = getLastMatchURL(file_path, pattern)
+lastURL = getLastMatchURL(filePath, pattern)
 
-if last_url:
-    subprocess.run(["xclip", "-selection", "clipboard"], input=last_url.encode(), check=True)
-    print("Copied to clipboard:", last_url)
+if lastURL:
+    subprocess.run(["xclip", "-selection", "clipboard"], input=lastURL.encode(), check=True)
+    print("Copied to clipboard:", lastURL)
 else:
     print("No matching URL found.")
